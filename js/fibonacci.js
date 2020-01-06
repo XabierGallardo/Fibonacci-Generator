e67d0db4250c5827e2a65fda7cfacb676b07931d
XabierGallardo <javiergallard91@gmail.com>
2020-01-06 02:16:25 +0000

    Finished initial functionality

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,22 +1,23 @@
-$(function() {
+window.onload = function() {
+
 	var fibonacci = [0,1];
-	var sequences = $("#sequences").val();
+	$("#generate").click(startSequence);
 
-	$("#generate").click(function () {
-		alert("test");
-		if (sequences <= 0) {
-		console.log("invalid number");
+	function startSequence () {
 
-		} else {
+	if ($("#sequences").val() <= 0) {
+		$("#numbers").html("Invalid number");
 
-			for (var i = 0; i<sequences; i++) {
+	} else {
+
+		for (var i = 0; i < $("#sequences").val(); i++) {
 
 		    var a = fibonacci[fibonacci.length - 2];
 		    var b = fibonacci[fibonacci.length - 1];
 		    var c = a + b;
 		    fibonacci.push(c);
-		  	}
-		  	$("#generate").html(fibonacci);
+	  	}
+	  	$("#numbers").html(fibonacci.join(" "));
 		}
-	});
-});
+	}	
+}
